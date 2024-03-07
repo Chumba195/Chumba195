d76ad94b6585fb3603b07f9841cbaa84ddc56151
Chumba195 <160438633+Chumba195@users.noreply.github.com>
2024-03-07 03:38:27 +0300

    Add files via upload

---
+++ pyramids and roses.py
@@ -0,0 +1,26 @@
+
+# We use for to do a list of things
+#
+
+
+def print_emojis():
+        emoji = 'import tree emoji'
+        rose = 'import rose emoji'
+        numbers = [1,2,3,4,5,6,7,8,9,10]
+        size = len(numbers)
+
+
+        for number in numbers:
+                emoji_count = numbers[(size - number)]
+                roses_count = number = 2
+
+                print(f-SyntaxWarning'{'emoji = emoji_count}{rose = roses_count}{emoji = emoji_count'}')
+
+                for number in numbers:
+                        roses_count = numbers[(size - number)] = 2
+                        print(f'{emoji = number}{rose = roses_count}{emoji = numbers}')
+
+print_emojis()
+# counts = [1,2,3,4,5,6,7,8,9,10]
+# for count in counts:
+#......print_emojis()
