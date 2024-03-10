0170a010342c54dc930fcb873356e2b11a0cd0dd
Lawi Kipchumba <160438633+Chumba195@users.noreply.github.com>
2024-03-10 19:02:37 +0300

    Add files via upload

---
+++ Append elmt.py
@@ -0,0 +1,8 @@
+names = ["Joseph", "Peter", "Cook", "Tim"]
+
+print('Current names list is:', names)
+
+new_name = input("Please enter a name:\n")
+names.append(new_name) #Using the append() function
+
+print('Updated name list is:', names)
